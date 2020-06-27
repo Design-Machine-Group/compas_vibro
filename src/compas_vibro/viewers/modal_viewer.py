5280e39bde88c59fc007d5bc7c4f46e66eee67d3
Tomas Mendez Echenagucia <tmendeze@uw.edu>
2020-06-27 16:31:10 -0700

    modal viewer object

--- src/compas_vibro/viewers/modal_viewer.py
+++ src/compas_vibro/viewers/modal_viewer.py
@@ -12,17 +12,27 @@ import plotly.graph_objects as go
 
 from compas.datastructures import Mesh
 
-def plot_mesh_lines(structure):
-    vertices = [structure.nodes[vk].xyz() for vk in structure.nodes]
-    faces = [structure.elements[ek].nodes for ek in structure.elements]
-    mesh = Mesh.from_vertices_and_faces(vertices, faces)
-    edges = [[mesh.vertex_coordinates(u), mesh.vertex_coordinates(v)] for u,v in mesh.edges()]
-    line_marker = dict(color='#0066FF', width=2)
-    lines = []
-    for u, v in edges:
-        lines.append(go.Scatter3d(x=(u[0], v[0]), y=(u[1], v[1]), z=(u[2], v[2]), mode='lines', line=line_marker))
-    fig = go.Figure(data=lines)
-    fig.show()
+class ModalViewer(object):
+    def __init__(self, structure):
+        self.structure  = structure
+        self.data       = []
+        self.layout     = None
+
+    def plot_mesh_lines(self):
+        vertices = [self.structure.nodes[vk].xyz() for vk in self.structure.nodes]
+        faces = [self.structure.elements[ek].nodes for ek in self.structure.elements]
+        mesh = Mesh.from_vertices_and_faces(vertices, faces)
+        edges = [[mesh.vertex_coordinates(u), mesh.vertex_coordinates(v)] for u,v in mesh.edges()]
+        line_marker = dict(color='#0066FF', width=2)
+        lines = []
+        for u, v in edges:
+            lines.append(go.Scatter3d(x=(u[0], v[0]), y=(u[1], v[1]), z=(u[2], v[2]), mode='lines', line=line_marker))
+        self.data.extend(lines)
+
+    
+    def show(self):
+        fig = go.Figure(data=self.data, layout=self.layout)
+        fig.show()
 
 if __name__ == "__main__":
     import os
@@ -33,6 +43,8 @@ if __name__ == "__main__":
 
     filepath = os.path.join(compas_vibro.DATA, 'vibro_test.obj')
     s = Structure.from_obj(filepath)
-    plot_modal(s)
+    v = ModalViewer(s)
+    v.plot_mesh_lines()
+    v.show()
 
 
