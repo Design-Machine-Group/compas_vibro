c25bc5a981345c38665116c2c05b2142920db0c0
Tomas Mendez Echenagucia <tmendeze@uw.edu>
2020-06-27 17:18:07 -0700

    modal shapes plot

--- src/compas_vibro/viewers/modal_viewer.py
+++ src/compas_vibro/viewers/modal_viewer.py
@@ -16,10 +16,48 @@ class ModalViewer(object):
     def __init__(self, structure):
         self.structure  = structure
         self.data       = []
-        self.layout     = None
+        self.make_layout()
 
-    def plot_mesh_lines(self):
-        vertices = [self.structure.nodes[vk].xyz() for vk in self.structure.nodes]
+    def make_layout(self):
+        layout = go.Layout(title='Modal Plot',
+                           scene=dict(xaxis=dict(
+                                      gridcolor='rgb(255, 255, 255)',
+                                      zerolinecolor='rgb(255, 255, 255)',
+                                      showbackground=True,
+                                      backgroundcolor='rgb(230, 230,230)'),
+                          yaxis=dict(gridcolor='rgb(255, 255, 255)',
+                                     zerolinecolor='rgb(255, 255, 255)',
+                                     showbackground=True,
+                                     backgroundcolor='rgb(230, 230,230)'),
+                          zaxis=dict(gridcolor='rgb(255, 255, 255)',
+                                     zerolinecolor='rgb(255, 255, 255)',
+                                     showbackground=True,
+                                     backgroundcolor='rgb(230, 230,230)')
+                            ),
+                          showlegend=False,
+                            )
+        self.layout = layout
+
+    def show(self):
+        fig = go.Figure(data=self.data, layout=self.layout)
+        fig.show()
+
+    def plot_modal_shape(self, mode):
+
+        s = 1.
+        vertices = []
+        nodes = sorted(self.structure.nodes.keys(), key=int)
+        for vk in nodes:
+            x, y, z = self.structure.nodes[vk].xyz()
+            dx = self.structure.results['modal'][mode].displacements['ux'][vk]
+            dy = self.structure.results['modal'][mode].displacements['uy'][vk]
+            dz = self.structure.results['modal'][mode].displacements['uz'][vk]
+            print(dx, dy, dz)
+            xyz = [x + dx * s, y + dy * s, z + dz * s]
+            xyz = [x * s, y * s, z * s]
+            vertices.append(xyz)
+            # vertices.append([x, y, z])
+
         faces = [self.structure.elements[ek].nodes for ek in self.structure.elements]
         mesh = Mesh.from_vertices_and_faces(vertices, faces)
         edges = [[mesh.vertex_coordinates(u), mesh.vertex_coordinates(v)] for u,v in mesh.edges()]
@@ -29,10 +67,17 @@ class ModalViewer(object):
             lines.append(go.Scatter3d(x=(u[0], v[0]), y=(u[1], v[1]), z=(u[2], v[2]), mode='lines', line=line_marker))
         self.data.extend(lines)
 
-    
-    def show(self):
-        fig = go.Figure(data=self.data, layout=self.layout)
-        fig.show()
+    def plot_supports(self):
+        dots = []
+        for dk in self.structure.displacements:
+            nodes = self.structure.displacements[dk].nodes
+            x = [self.structure.nodes[nk].x for nk in nodes]
+            y = [self.structure.nodes[nk].y for nk in nodes]
+            z = [self.structure.nodes[nk].z for nk in nodes]
+            dots.append(go.Scatter3d(x=x, y=y, z=z, mode='markers'))
+        self.data.extend(dots)
+
+
 
 if __name__ == "__main__":
     import os
@@ -44,7 +89,8 @@ if __name__ == "__main__":
     filepath = os.path.join(compas_vibro.DATA, 'vibro_test.obj')
     s = Structure.from_obj(filepath)
     v = ModalViewer(s)
-    v.plot_mesh_lines()
+    v.plot_modal_shape(0)
+    v.plot_supports()
     v.show()
 
 
