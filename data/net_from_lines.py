eac9dad29280e2eec57e5b4dede81584541537e1
Tomas Mendez Echenagucia <tmendeze@uw.edu>
2020-07-03 10:45:32 -0700

    data changes

---
+++ data/net_from_lines.py
@@ -0,0 +1,10 @@
+import rhinoscriptsyntax as rs
+import compas
+
+# from compas.datastructures import Network
+#
+# lines = rs.ObjectsByLayer('lines')
+# lines = [[rs.CurveStartPoint(l), rs.CurveEndPoint(l)] for l in lines]
+#
+# net = Network.from_lines(lines)
+# net.to_json('network_10x10.json')
