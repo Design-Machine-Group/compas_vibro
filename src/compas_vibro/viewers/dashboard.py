a5f0e276af04508f40777ccf67946e14fbde1942
Tomas Mendez Echenagucia <tmendeze@uw.edu>
2022-02-25 16:32:58 -0800

    removing depracated imports

--- src/compas_vibro/viewers/dashboard.py
+++ src/compas_vibro/viewers/dashboard.py
@@ -1,7 +1,6 @@
 import math
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import html, dcc
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 from plotly.subplots import make_subplots
